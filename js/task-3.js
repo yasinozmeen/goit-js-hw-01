09b0a97f58ea8722f48cd92b2d7f21548ed50030
Yasin Özmen <yasinozmeen3@gmail.com>
2025-05-07 18:53:49 +0300

    Refactor getElementWidth function to use Math.ceil for accurate width calculation

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function getElementWidth(content, padding, border) {
   const sonuc =
-    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+    Math.ceil(parseFloat(content)) +
+    Math.ceil(2 * parseFloat(padding)) +
+    Math.ceil(2 * parseFloat(border));
   return sonuc;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.4px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
